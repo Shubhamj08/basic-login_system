3b9b2ff6e5202b9a47a3c021383ba1184109a8e6
Shubhamj08 <46774429+Shubhamj08@users.noreply.github.com>
2020-07-02 02:10:56 +0530

    ui almost ready

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>
+    login system
+  </title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+  <link href="header_style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-x1 fixed-top bg-light">
+    <span class="nav-item navbar-brand" style="color:black;"><b>Hi</b></span>
+    <span class="navbar-text abs-center-x"><b>This is an example of a basic login system</b></span>
+    <ul class="nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="login_page.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="signUp_page.php">SignUp</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">LogOut</a>
+      </li>
+    </ul>
+  </nav>
+</body>
+
+</html>
---
+++ login_page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+  <?php
+  include "header.php";
+  ?>
+  <div class=" d-flex justify-content-center mt-5">
+    <div class="jumbotron w-25">
+      <h4 class="display 3">Enter Login Credentials :</h4>
+        <form class="form-group" action="" method="post">
+          <label for="username">Username :</label>
+          <input type="text" class="form-control" id="username" name="username"/>
+          <label for="password">Password :</label>
+          <input type="password" class="form-control" id="password" name="password"/>
+          <button class="btn btn-primary my-3 btn-sm" type="submit">Submit</button>
+        </form>
+    </div>
+  </div>
+
+</body>
+</html>
